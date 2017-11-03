18ef3897ae2b6cdad0982e297411bb7dd59682d8
sjstef <sjstef@gmail.com>
2017-11-03 14:10:52 -0400

    Use strong params in session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SessionsController < ApplicationController
   def create
-    @user = User.find_by_credentials(params[:email], params[:password])
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
     if @user
       login!(@user)
       render "api/users/show"
