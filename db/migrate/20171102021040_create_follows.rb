c4d53214052816fb1a037d1be13dafd37db8d853
sjstef <sjstef@gmail.com>
2017-11-03 12:29:08 -0400

    Fix database syntax

--- db/migrate/20171102021040_create_follows.rb
+++ db/migrate/20171102021040_create_follows.rb
@@ -1,8 +1,8 @@
 class CreateFollows < ActiveRecord::Migration[5.1]
   def change
     create_table :follows do |t|
-      t.integer, :follower_id, null: false
-      t.integer, :following_id, null: false
+      t.integer :follower_id, null: false
+      t.integer :following_id, null: false
 
       t.timestamps
     end
